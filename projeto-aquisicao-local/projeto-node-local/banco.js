830f841eb139480e4c2cf19a90a91876459c370e
diegocrocha <diego.rocha@bandtec.com.br>
2020-06-10 16:59:05 -0300

    ajeitando de acordo com Yoshi

---
+++ projeto-aquisicao-local/projeto-node-local/banco.js
@@ -0,0 +1,33 @@
+const configuracoes = {
+    banco: {
+        server: "serverprojecteva.database.windows.net",
+        user: "adminlocal",
+        password: "#Gfevasprint2",
+        database: "bd_eva",
+        options: {
+            encrypt: true
+        },
+        pool: {
+            max: 4,
+            min: 1,
+            idleTimeoutMillis: 30000,
+            connectionTimeout: 5000
+        }
+    }
+}
+ 
+const sql = require('mssql');
+sql.on('error', err => {
+    console.error(`Erro de Conexão: ${err}`);
+});
+
+
+function conectar() {
+  sql.close();
+  return sql.connect(configuracoes.banco)
+} 
+
+module.exports = {
+    conectar: conectar,
+    sql: sql
+}
