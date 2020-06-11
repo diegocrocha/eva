1ff42a3066d220ce4715207ef4ec6985b0ccc76d
diegocrocha <diego.rocha@bandtec.com.br>
2020-06-11 12:40:08 -0300

    login yoshi

---
+++ projeto-site/config/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  development: {
+    dialect: "sqlite",
+    storage: "./db.development.sqlite"
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: 'adminlocal',
+    password: '#Gfevasprint2',
+    database: 'bd_eva',
+    host: 'serverprojecteva.database.windows.net',
+    dialect: 'mssql',
+    xuse_env_variable: 'DATABASE_URL',
+    dialectOptions: {
+      options: {
+        encrypt: true
+      }
+    },
+    pool: { 
+      max: 5,
+      min: 1,
+      acquire: 5000,
+      idle: 30000,
+      connectTimeout: 5000
+    }
+  }
+};
+ 
